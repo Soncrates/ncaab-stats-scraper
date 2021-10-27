741ae547c11a259ee58c0331405e5a89fdcbc15f
Soncrates <emersoncus@yahoo.com>
2021-10-27 02:11:04 -0400

    Update create_box_scores.py

--- create_box_scores.py
+++ create_box_scores.py
@@ -26,20 +26,19 @@ def step02_transform(link) :
     return str(BASE.base_url  + link.get('href'))
 def step03_extract_box_scores_by_team(soup_table) :
     table_body = soup_table.find('tbody')
-    team_name = table_body.find('tr', attrs={'class':'heading'}).find('td').get_text()
-    column_list = [ col.get_text() for col in table_body.find('tr', attrs={'class':'grey_heading'}).find_all('th') ]
+    team_name = table_body.find('tr', attrs={'class':'heading'}).find('td').text.strip()
+    column_list = [ col.text.strip() for col in table_body.find_all('tr', attrs={'class':'grey_heading'})[-1].find_all('th') ]
     row_list = [ ele.text.strip() for ele in cols for cols in row.find_all('td') for row in table_body.find_all('tr', attrs={'class':'smtext'})]
     return team_name, column_list, row_list
 def step03_extract_box_scores(url) :
     response = POST(url, BASE.params, BASE.headers)
     soap = BeautifulSoup(response)
     return soup.find_all('table', attrs={'class':'mytable'})
-def by_sport(sport) :
-    team_list = step01_read_team_list(sport)
+def by_sport(filename) :
+    team_list = step01_read_team_list(filename)
     box_scores = { name : step02_parse_response_for_box_scores(response) in name, response in step02_extract_team_data(**team_list) }
-    box_score_list = box_scores.values()
-    box_score_list = list(set(box_score_list))
-    box_score_list = [ step03_extract_box_scores_by_team(table) for table in step03_extract_box_scores(box_score) for box_score in box_score_list ]
+    box_score_list = list(set(box_scores.values()))
+    box_score_list = [ step03_extract_box_scores_by_team(table) for table in table_list for table_list in step03_extract_box_scores(url) for url in box_score_list ]
         
 def main() :
     for sport in glob("*csv") :
