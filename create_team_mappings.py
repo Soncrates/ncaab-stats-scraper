d78e2df86337ee571ba98057d29e30cdd7c82e18
Soncrates <emersoncus@yahoo.com>
2021-10-26 22:16:33 -0400

    Update create_team_mappings.py

--- create_team_mappings.py
+++ create_team_mappings.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python
 ##############################################################
-# Program name: NCAA Basketball Stats Scraper (Team Mappings Module)
+# Program name: NCAA Stats Scraper (Team Mappings Module)
 # Version: 1.0
-# By: Rodrigo Zamith
-# License: MPL 2.0 (see LICENSE file in root folder)
-# Additional thanks: 
 ##############################################################
 
 # Import modules and libraries
-import scraperfunctions
-import scrapersettings
+from scraperfunctions import grabber
+from scrapersettings import Lacrosse, Football, Basketball, Soccer, SportBase
 from bs4 import BeautifulSoup
+
+sports = [Lacrosse(),Football(),Basketball(),Soccer()]
+
+def step01(sport) :
+    teamlist_data = scraperfunctions.grabber(scrapersettings.start_url, scrapersettings.params, scrapersettings.http_header) # Get data from main page
+    teamlist_data_soup = BeautifulSoup(teamlist_data) # Soupify that data
+
+    for link in teamlist_data_soup.find_all('a'): # For each hyperlink on the page
+        if "team/index/" + str(scrapersettings.year_index) + "?org_id=" in link.get('href'): # If the hyperlink contains this string (limiting it only to team pages)
+            team_id = str(link.get('href').split("team/index/" + str(scrapersettings.year_index) + "?org_id=")[1]) # Get the team ID from the URL
+            team_name = str(link.get_text()) # Get the text associated with the hyperlink
+            team_url = str(scrapersettings.domain_base + link.get('href')) # Get the URL and append the base domain
+            team_mappingfile_w.writelines(str(team_id) + "\t" + str(team_name) + "\t" + str(team_url) + "\n") # Add lines to our TSV file for archival purposes
+
 
 if (scrapersettings.map_teams == 1):
     print "Generating team mappings"
