b8d34752f669322105936e8b147c348100556c42
Soncrates <emersoncus@yahoo.com>
2021-10-28 11:41:33 -0400

    Update create_team_mappings.py

--- create_team_mappings.py
+++ create_team_mappings.py
@@ -6,7 +6,7 @@
 
 # Import modules and libraries
 from scraperfunctions import grabber as EXTRACT
-from scrapersettings import Lacrosse, Football, Basketball, Soccer, SportExtract as BASE
+from scrapersettings import SportExtract as BASE
 from bs4 import BeautifulSoup as TRANSFORM
 
 def step01_request_team_list(sport,**kvargs) :
@@ -20,22 +20,22 @@ def step02_test(link) :
     return ctx.startswith('/team/') and 'division' not in ctx
 def step02_transform(link) :
     return str(link.get_text()),  str(BASE.base_url  + link.get('href'))
-def team_list_by_sport(*url_by_division_list) :
-    team_list_by_division_response_list = [ POST(url, BASE.params, BASE.headers) for url in url_by_division_list]  
-    ret = [ step02_parse_response(team_list) for team_list in team_list_by_division_response_list ]
-    return { k: v for k, v in team_list.items() for team_list in ret }
+def merge_divisions(*division_list) :
+    return { k: v for k, v in division.items() for division in division_list }
 def write_csv(filename,team_list) :
     with open(filename,'w') as f:
          f.writelines("team_name\tteam_url\n")
          for name, url in team_list.items() :
              f.writelines("{}\t{}\n".format(name,url))
-def main() :
-    sport_list = [Lacrosse(),Football(),Basketball(),Soccer()]
-    team_list = [ step01_request_team_list(sport) for sport in sport_list ]
-
-    sport_list = { "team_list_{sport_code}.csv".format(**sport.default_params) : step01_request_team_list(sport) for sport in sport_list }
-    for filename, team_list in sport_list.items() :
-        write_csv(filename, team_list)
+def main(*sport_list) :
+    name_list =  [ "team_list_{sport_code}.csv".format(**sport.default_params) for sport in sport_list ]
+    division_list = [ step01_request_team_list(sport) for sport in sport_list ]
+    for i, name in enumerate(name_list) :
+        division_list = [ step02_parse_response(division) for division in division_list[i] ]
+        team_list = merge_divisions(*division_list)
+        write_csv(name_list[i], team_list)        
 
 if __name__ == "__main__" :
-   main()
+   from scrapersettings import Lacrosse, Football, Basketball, Soccer
+   sport_list = [Lacrosse(),Football(),Basketball(),Soccer()]
+   main(*sport_list)
