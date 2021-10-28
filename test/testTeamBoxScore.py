c3b64775edab07004b09758a33c68ee12cd61ded
Soncrates <emersoncus@yahoo.com>
2021-10-28 15:19:59 -0400

    Update testTeamBoxScore.py

--- test/testTeamBoxScore.py
+++ test/testTeamBoxScore.py
@@ -25,8 +25,8 @@ class TestScrapeBoxScore(unittest.TestCase) :
         log.debug(sorted(obj))
     def testLacrosseStep03(self) :
         obj = [ TEST.step03_transform_box_scores(team_list) for team_list in [self.team_MLA_Navy_BoxSCore] ]
-        log.debug(COMMON.pretty_print(obj))
-        log.debug(sorted(obj))
+        obj = {k,v for score in obj for k,v in score.items() }
+        log.debug(obj)
 
 if __name__ == '__main__' :
    import sys
