145cfd692d0a7513b2818596ab803bc9e9c59850
Soncrates <emersoncus@yahoo.com>
2021-10-28 02:51:53 -0400

    Update scraperfunctions.py

--- scraperfunctions.py
+++ scraperfunctions.py
@@ -12,14 +12,12 @@ except:
 
 try:
     # For Python 3.0 and later
-    from urllib.request import urlopen, URLError, HTTPCookieProcessor, build_opener
-    from urllib.parse import urlencode
+    from urllib3.request import urlopen, URLError, HTTPCookieProcessor, build_opener
+    from urllib3.parse import urlencode
 except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import urlopen, URLError, HTTPCookieProcessor, build_opener
     from urllib import urlencode
-import re
-import scrapersettings
 from libDecorator import retry
 
 def create_cookie():
@@ -38,15 +36,3 @@ def grabber(url, params, http_header):
     res = cookiejar.open(req)
     data = res.read()
     return(data)
-
-def get_team_mappings():
-    team_map = open(scrapersettings.team_mappingfile, "rb")
-    team_map = team_map.readlines()[1:]
-    team_map = dict([(var.split("\t")[0], (var.split("\t")[1], var.split("\t")[2].strip("\n"))) for var in team_map])
-    return(team_map)
-
-def get_game_mappings() :
-    game_map = open(scrapersettings.schedule_mappingfile, "rb")
-    game_map = game_map.readlines()[1:]
-    game_map = dict([(var.split("\t")[0], (var.split("\t")[1], var.split("\t")[2], var.split("\t")[3], var.split("\t")[4], var.split("\t")[5].strip("\n"))) for var in game_map])
-    return(game_map)
