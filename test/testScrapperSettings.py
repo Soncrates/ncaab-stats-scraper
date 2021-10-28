aea41f13eb4847870abf0033cb7f2825d4093b35
Soncrates <emersoncus@yahoo.com>
2021-10-28 00:23:47 -0400

    Update testScrapperSettings.py

--- test/testScrapperSettings.py
+++ test/testScrapperSettings.py
@@ -27,5 +27,6 @@ if __name__ == '__main__' :
    log_file = COMMON.build_args(*sys.argv).replace('.py','') + '.log'
    log_file = COMMON.build_path('../log',log_file)
    COMMON.remove_file(log_file)
-   log.basicConfig(filename=log_file,stream = sys.stdout, format=COMMON.LOG_FORMAT_TEST, level=log.DEBUG)
+   # log.basicConfig(filename=log_file, format=COMMON.LOG_FORMAT_TEST, level=log.DEBUG)
+   log.basicConfig(stream = sys.stdout, format=COMMON.LOG_FORMAT_TEST, level=log.DEBUG)
    unittest.main()
