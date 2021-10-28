f5a0473f8094ddaa648ff97b437cc09dfa84e40c
Soncrates <emersoncus@yahoo.com>
2021-10-28 01:21:23 -0400

    Update and rename testScraperFunctions.py to testTeamMapping.py

--- test/testTeamMapping.py
+++ test/testTeamMapping.py
@@ -2,19 +2,23 @@ from sys import path
 path.append("../")
 
 import libCommon as COMMON
-import scraperfunctions as TEST
+from scrapersettings import Lacrosse, Football, Basketball, Soccer, SportExtract as BASE
+import create_team_mappings as TEST
 import unittest
 import logging as log
 
 class TestScraperFunctions(unittest.TestCase) :
-    def setUp(self) : 
-        COMMON.mkdir("../log")
+    def setUp(self) : pass
+    def testLacrosseStep01(self) :
+        obj = TEST.team_list_by_sport(Lacrosse().extract_team_list())
+        log.debug(obj)
 
 if __name__ == '__main__' :
    import sys
    log_file = COMMON.build_args(*sys.argv).replace('.py','') + '.log'
    log_file = COMMON.build_path('../log',log_file)
    COMMON.remove_file(log_file)
+   COMMON.mkdir("../log")
    print(log_file)
    log.basicConfig(filename=log_file, format=COMMON.LOG_FORMAT_TEST, level=log.DEBUG)
    #log.basicConfig(stream = sys.stdout, format=COMMON.LOG_FORMAT_TEST, level=log.DEBUG)
