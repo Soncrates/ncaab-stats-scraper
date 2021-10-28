6c70cf4073179db130c0be4ebb93354db2fafce8
Soncrates <emersoncus@yahoo.com>
2021-10-28 03:16:55 -0400

    Update scraperfunctions.py

--- scraperfunctions.py
+++ scraperfunctions.py
@@ -3,6 +3,7 @@
 # Program name: NCAA Stats Scraper (Functions Module)
 # Version: 1.0
 ##############################################################
+import logging as log
 try:
     # For Python 3.0 and later
     import http.cookiejar as cookielib
@@ -15,12 +16,17 @@ except ImportError:
     from urllib import urlencode
 from libDecorator import retry
 
+LOG = logging.getLogger(__name__) 
+
 def create_cookie():
     cookie_jar = cookielib.LWPCookieJar()
     ret = HTTPCookieProcessor(cookie_jar)
     return build_opener(ret)
 @retry(URLError, tries=4, delay=3, backoff=2)
 def grabber(url, params, http_header):
+    print(url)
+    print(params)
+    print(http_header)
     req = POST(url, urlencode(params).encode('utf-8'), http_header)
     #res = create_cookie().open(req)
     #data = res.read()
