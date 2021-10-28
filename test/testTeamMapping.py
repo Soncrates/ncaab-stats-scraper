11bfaa88e4b35460832f4d2649ee73d16631f957
Soncrates <emersoncus@yahoo.com>
2021-10-28 09:22:20 -0400

    Update testTeamMapping.py

--- test/testTeamMapping.py
+++ test/testTeamMapping.py
@@ -7,12 +7,15 @@ import create_team_mappings as TEST
 import unittest
 import logging as log
 
+
+def load_response(filename) :
+    filename_list = COMMON.find_files(filename)
+    with open(filename_list[0]) as fp :
+         return "".join(fp.readlines())
 class TestScraperFunctions(unittest.TestCase) :
     team_list_MLA = None
     def setUp(self) : 
-        self.team_list_MLA = COMMON.find_files("./test_input/team_list_MLA.html")
-        with open(self.team_list_MLA[0]) as fp :
-            self.team_list_MLA = fp.readlines()
+        self.team_list_MLA = load_response("./test_input/team_list_MLA.html")
     def dep_testLacrosseStep01(self) :
         obj = TEST.team_list_by_sport(*Lacrosse().url_team_list())
         log.debug(obj)
