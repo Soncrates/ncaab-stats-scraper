141ea5babe1d7ed3e86ce15f8659d0b3611a3d0d
Soncrates <emersoncus@yahoo.com>
2021-10-28 20:55:12 -0400

    Update create_box_scores.py

--- create_box_scores.py
+++ create_box_scores.py
@@ -16,7 +16,12 @@ def step01_read_team_list(filename):
 def step02_extract_team_data(*team_list):
     return [ EXTRACT(url, BASE.params, BASE.headers) for url in team_list ]
 def step02_parse_response_for_box_scores(response) :
-    link_list = TRANSFORM(response,features="html.parser").findAll('a')
+    table = TRANSFORM(response,features="html.parser").find('tbody')
+    row_list = tavle.findAll('tr')
+    td_list = [ row.findAll('td') for row in row_list ]
+    td_list = extract_table_rows(td_list)
+    print(td_list)
+    link_list = table.findAll('a')
     return [ step02_transform(link) for link in link_list if step02_test(link) ]
 def step02_test(link) :
     return link.get('href').endswith('box_score')
@@ -28,11 +33,9 @@ def step03_transform_box_scores(response) :
     return [ step03_transform_table(table) for table in table_list if step03_test(table) ]
 def step03_test(table) :
     tr_list = table.findAll('tr', attrs={'class':'grey_heading'})
-    td_list_of_list = [ tr.findAll('td') for tr in tr_list ]
-    test = []
-    for td_list in td_list_of_list :
-        test.extend(td_list)
-    td_list = [ td.text.strip() for td in td_list if td.text.strip() in ['1st Quarter', '2nd Quarter', '3rd Quarter', '4th Quarter'] ]
+    td_list = [ tr.findAll('td') for tr in tr_list ]
+    td_list = flatten_table_rows(td_list)
+    td_list = [ td for td in td_list if td in ['1st Quarter', '2nd Quarter', '3rd Quarter', '4th Quarter'] ]
     if len(td_list) > 0 :
         return False
     return True
@@ -40,19 +43,25 @@ def step03_transform_table(soup_table) :
     team_name = soup_table.find('tr', attrs={'class':'heading'}).find('td').text.strip()
     grey_header_list = soup_table.findAll('tr', attrs={'class':'grey_heading'})
     column_list = [ col.text.strip() for col in grey_header_list[0].findAll('th') ]
+
     row_list = soup_table.findAll('tr', attrs={'class':'smtext'})
-    row_list = step03_transform_rows(row_list)
+    td_list = [ tr.findAll('td') for tr in row_list ]
+    row_list = extract_table_rows(td_list)
+
     total = grey_header_list[-1]
     row_list.extend(step03_transform_rows([ total ]))
     ret = PY.DataFrame(row_list,columns=column_list)
     return { team_name : ret }
-def step03_transform_rows(soup_row_list) :
-    td_list = [ tr.findAll('td') for tr in soup_row_list ]
+def extract_table_rows(tr_list) :    
     ret = []
-    for row in td_list :
-        ret.append([ ele.text.strip() for ele in row ])
+    for td_list in tr_list :
+        ret.append([ ele.text.strip() for ele in td_list ])
     return ret
-    
+def flatten_table_rows(tr_list) :    
+    ret = []
+    for td_list in tr_list :
+        ret.extend([ ele.text.strip() for ele in td_list ])
+    return ret
 def by_sport(filename) :
     team_url_list = step01_read_team_list(filename).values()
     box_scores = [ step02_parse_response_for_box_scores(response) for response in step02_extract_team_data(*team_url_list) ]
