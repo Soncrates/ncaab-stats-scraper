42bec8ce9adfb647d2ae064c9cf79fa079127466
Soncrates <emersoncus@yahoo.com>
2021-10-31 00:45:29 -0400

    Update scraperfunctions.py

--- scraperfunctions.py
+++ scraperfunctions.py
@@ -9,6 +9,13 @@ from libDecorator import retry
 
 LOG = logging.getLogger(__name__) 
 
+try:
+    # For Python 3.0 and later
+    from urllib.request import URLError
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import URLError
+
 @retry(URLError, tries=4, delay=3, backoff=2)
 def grabber(url, params, http_header):
     r = requests.get(url,http_header)
