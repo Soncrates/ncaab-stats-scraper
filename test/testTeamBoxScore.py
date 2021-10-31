b8bcb8cc0d86a510d6b8238abd3aeda6935d5771
Soncrates <emersoncus@yahoo.com>
2021-10-31 00:23:56 -0400

    Update testTeamBoxScore.py

--- test/testTeamBoxScore.py
+++ test/testTeamBoxScore.py
@@ -1,8 +1,6 @@
 from sys import path
 path.append("../")
-
 import libCommon as COMMON
-from scrapersettings import Lacrosse, Football, Basketball, Soccer, SportExtract as BASE
 import create_box_scores as TEST
 import unittest
 import logging as log
@@ -11,13 +9,13 @@ def load_response(filename) :
     filename_list = COMMON.find_files(filename)
     with open(filename_list[0]) as fp :
          return "".join(fp.readlines())
+
 class TestScrapeBoxScore(unittest.TestCase) :
     team_MLA_Navy = None
     team_MLA_Navy_BoxSCore = None
     def setUp(self) : 
         self.team_MLA_Navy = load_response("./test_input/team_MLA_Navy.html")
         self.team_MLA_Navy_BoxSCore = load_response("./test_input/team_MLA_BoxScore.html")
-        
     def testLacrosseStep02(self) :
         obj = [ TEST.step02_parse_response_for_box_scores(team_list) for team_list in [self.team_MLA_Navy] ]
         log.debug(COMMON.pretty_print(obj))
@@ -28,14 +26,11 @@ class TestScrapeBoxScore(unittest.TestCase) :
             obj.to_csv(r'./test_output.csv', index=False)
             f = load_response(r'./test_output.csv')
             log.debug(f)
-
 if __name__ == '__main__' :
    import sys
    log_file = COMMON.build_args(*sys.argv).replace('.py','') + '.log'
    log_file = COMMON.build_path('../log',log_file)
    COMMON.remove_file(log_file)
    COMMON.mkdir("../log")
-   print(log_file)
    log.basicConfig(filename=log_file, format=COMMON.LOG_FORMAT_TEST, level=log.DEBUG)
-   #log.basicConfig(stream = sys.stdout, format=COMMON.LOG_FORMAT_TEST, level=log.DEBUG)
    unittest.main()
